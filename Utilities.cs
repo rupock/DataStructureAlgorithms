e2100b77e6f3b84dd51b998bef56b5fa748554cc
Ahmed Chowdhury <rupock@yahoo.com>
2023-06-24 07:13:38 -0400

    utilities.cs

---
+++ Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataStructureAlgorithms
+{
+	public static class Utilities
+	{
+		public static int GetNumberFromInput()
+		{
+			while (true)
+			{
+				string input = Console.ReadLine()!;
+
+				if (int.TryParse(input, out int number))
+				{
+					return number;
+				}
+
+				Console.WriteLine("Invalid input. Please enter a valid number: ");
+			}
+		}
+	}
+}
