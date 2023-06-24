04e9655979d3c3d6fe6e4914980f9dbfc8cb30ab
Ahmed Chowdhury <rupock@yahoo.com>
2023-06-24 06:41:21 -0400

    Program.cs and Readme.md  added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+
+namespace StackDataStructureAlgorithms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StackArray st = new StackArray();
+         
+			st.main();
+		
+			Console.WriteLine("----QUEUE----");
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+﻿Data structure and algorithms
+
+This program written in .net 5, now converted to 7.0
+
+There will be series of data structure and algorithms related programs.
+
+
